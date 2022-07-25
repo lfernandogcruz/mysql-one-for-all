e35a5bbdd16b140718fd3aa1a8dbcf0c63ad783d
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-07-25 15:08:51 -0300

    fix: req 01 - create and fills databese

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ CREATE DATABASE SpotifyClone;
 CREATE TABLE SpotifyClone.planos(
     plano_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     plano_titulo VARCHAR(20) NOT NULL,
-    plano_valor FLOAT(4,2) NOT NULL,
+    plano_valor DECIMAL(3,2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.users(
@@ -14,12 +14,12 @@ CREATE TABLE SpotifyClone.users(
     user_age INT(3) NOT NULL,
     user_plano INT NOT NULL,
     user_subscription_date DATE NOT NULL,
-    FOREIGN KEY (user_plano) REFERENCES SpotifyClone.planos (plano_id),
+    FOREIGN KEY (user_plano) REFERENCES SpotifyClone.planos (plano_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artists(
     artist_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    artist_name VARCHAR(50) NOT NULL,
+    artist_name VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.album(
@@ -27,7 +27,7 @@ CREATE TABLE SpotifyClone.album(
     album_title VARCHAR(50) NOT NULL,
     album_artist INT NOT NULL,
     album_release_year YEAR NOT NULL,
-    FOREIGN KEY (album_artist) REFERENCES SpotifyClone.artists (artist_id),
+    FOREIGN KEY (album_artist) REFERENCES SpotifyClone.artists (artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.tracks(
@@ -35,45 +35,45 @@ CREATE TABLE SpotifyClone.tracks(
     track_title VARCHAR(50) NOT NULL,
     track_album INT NOT NULL,
     track_duration INT(4) NOT NULL,
-    FOREIGN KEY (track_album) REFERENCES SpotifyClone.album (album_id),
+    FOREIGN KEY (track_album) REFERENCES SpotifyClone.album (album_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.play_history(
     user_id INT NOT NULL,
     track_id INT NOT NULL,
     reproduction_date DATETIME NOT NULL,
-    CONSTRAINT PRIMARY KEY (user_id, track_id)
+    CONSTRAINT PRIMARY KEY (user_id, track_id),
     FOREIGN KEY (user_id) REFERENCES SpotifyClone.users (user_id),
-    FOREIGN KEY (track_id) REFERENCES SpotifyClone.tracks (track_id),
+    FOREIGN KEY (track_id) REFERENCES SpotifyClone.tracks (track_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.followed_artists(
     user_id INT NOT NULL,
     artist_id INT NOT NULL,
-    CONSTRAINT PRIMARY KEY (user_id, artist_id)
+    CONSTRAINT PRIMARY KEY (user_id, artist_id),
     FOREIGN KEY (user_id) REFERENCES SpotifyClone.users (user_id),
-    FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artists (artist_id),
+    FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artists (artist_id)
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.planos (plano_titulo, valor_plano)
+INSERT INTO SpotifyClone.planos (plano_titulo, plano_valor)
 VALUES
-  ("gratuito", 0),
+  ("gratuito", 0.00),
   ("familiar", 7.99),
   ("universitário", 5.99),
   ("pessoal", 6.99);
 
 INSERT INTO SpotifyClone.users (user_name, user_age, user_plano, user_subscription_date)
 VALUES
-  ("Thati", 23, 1, 2019-10-20),
-  ("Cintia", 35, 2, 2017-12-30),
-  ("Bill", 20, 3, 2019-06-05),
-  ("Roger", 45, 4, 2020-05-13),
-  ("Norman", 58, 4, 2017-02-17),
-  ("Patrick", 33, 2, 2017-01-06),
-  ("Vivian", 26, 3, 2018-01-05),
-  ("Carol", 19, 3, 2018-02-14),
-  ("Angelina", 42, 2, 2018-04-29),
-  ("Paul", 46, 2, 2017-01-17);
+  ("Thati", 23, 1, "2019-10-20"),
+  ("Cintia", 35, 2, "2017-12-30"),
+  ("Bill", 20, 3, "2019-06-05"),
+  ("Roger", 45, 4, "2020-05-13"),
+  ("Norman", 58, 4, "2017-02-17"),
+  ("Patrick", 33, 2, "2017-01-06"),
+  ("Vivian", 26, 3, "2018-01-05"),
+  ("Carol", 19, 3, "2018-02-14"),
+  ("Angelina", 42, 2, "2018-04-29"),
+  ("Paul", 46, 2, "2017-01-17");
 
 INSERT INTO SpotifyClone.artists (artist_name)
 VALUES
@@ -142,44 +142,44 @@ VALUES
 
 INSERT INTO SpotifyClone.play_history (user_id, track_id, reproduction_date)
 VALUES
-  (1, "Honey", "2020-02-28 10:45:55"),
-  (1, "Walking And Man", "2020-05-02 05:30:35"),
-  (1, "Young And Father", "2020-03-06 11:22:33"),
-  (1, "Diamond Power", "2020-08-05 08:05:17"),
-  (1, "Let's Be Silly", "2020-09-14 16:32:22"),
-  (2, "I Heard I Want To Bo Alone", "2020-01-02 07:40:33"),
-  (2, "Finding My Traditions", "2020-05-16 06:16:22"),
-  (2, "Without My Love", "2020-10-09 12:27:48"),
-  (2, "Baby", "2020-09-21 13:14:46"),
-  (3, "Magic Circus", "2020-11-13 16:55:13"),
-  (3, "Dance With Her Own", "2020-12-05 18:38:30"),
-  (3, "Hard And Time", "2020-07-30 10:00:00"),
-  (4, "Reflections Of Magic", "2021-08-15 17:10:10"),
-  (4, "I Ride Alone", "2021-07-10 15:20:30"),
-  (4, "Honey, I'm A Lone Wolf", "2021-01-09 01:44:33"),
-  (5, "Honey, So Do I", "2020-07-03 19:33:28"),
-  (5, "Rock His Everything", "2017-02-24 21:14:22"),
-  (5, "Diamond Power", "2020-08-06 15:23:43"),
-  (5, "Soul For Us", "2020-11-10 13:52:27"),
-  (6, "Wouldn't It Be Nice", "2019-02-07 20:33:48"),
-  (6, "He Heard You're Bad For Me", "2017-01-24 00:31:17"),
-  (6, "He Hopes We Can't Stay", "2017-10-12 12:35:20"),
-  (6, "Walking And Game", "2018-05-29 14:56:41"),
-  (7, "Time Fireworks", "2018-05-09 22:30:49"),
-  (7, "Troubles Of My Inner Fire", "2020-07-27 12:52:58"),
-  (7, "Celebration Of More", "2018-01-16 18:40:43"),
-  (8, "Baby", "2018-03-21 16:56:40"),
-  (8, "You Make Me Feel So..", "2020-10-18 13:38:05"),
-  (8, "He's Walking Away", "2019-05-25 08:14:03"),
-  (8, "He's Trouble", "2021-08-15 21:37:09"),
-  (9, "Thang Of Thunder", "2021-05-24 17:23:45"),
-  (9, "Words Of Her Life", "2018-12-07 22:48:52"),
-  (9, "Sweetie, Let's Go Wild", "2021-03-14 06:14:26"),
-  (9, "She Knows", "2020-04-01 03:36:00"),
-  (10, "History Of My Roses", "2017-02-06 08:21:34"),
-  (10, "Without My Love", "2017-12-04 05:33:43"),
-  (10, "Rock His Everything", "2017-07-27 05:24:49"),
-  (10, "Home Forever", "2017-12-25 01:03:57");
+  (1, 36, "2020-02-28 10:45:55"),
+  (1, 25, "2020-05-02 05:30:35"),
+  (1, 23, "2020-03-06 11:22:33"),
+  (1, 14, "2020-08-05 08:05:17"),
+  (1, 15, "2020-09-14 16:32:22"),
+  (2, 34, "2020-01-02 07:40:33"),
+  (2, 24, "2020-05-16 06:16:22"),
+  (2, 21, "2020-10-09 12:27:48"),
+  (2, 39, "2020-09-21 13:14:46"),
+  (3, 6, "2020-11-13 16:55:13"),
+  (3, 3, "2020-12-05 18:38:30"),
+  (3, 26, "2020-07-30 10:00:00"),
+  (4, 2, "2021-08-15 17:10:10"),
+  (4, 35, "2021-07-10 15:20:30"),
+  (4, 27, "2021-01-09 01:44:33"),
+  (5, 7, "2020-07-03 19:33:28"),
+  (5, 12, "2017-02-24 21:14:22"),
+  (5, 14, "2020-08-06 15:23:43"),
+  (5, 1, "2020-11-10 13:52:27"),
+  (6, 38, "2019-02-07 20:33:48"),
+  (6, 29, "2017-01-24 00:31:17"),
+  (6, 30, "2017-10-12 12:35:20"),
+  (6, 22, "2018-05-29 14:56:41"),
+  (7, 5, "2018-05-09 22:30:49"),
+  (7, 4, "2020-07-27 12:52:58"),
+  (7, 11, "2018-01-16 18:40:43"),
+  (8, 39, "2018-03-21 16:56:40"),
+  (8, 40, "2020-10-18 13:38:05"),
+  (8, 32, "2019-05-25 08:14:03"),
+  (8, 33, "2021-08-15 21:37:09"),
+  (9, 16, "2021-05-24 17:23:45"),
+  (9, 17, "2018-12-07 22:48:52"),
+  (9, 8, "2021-03-14 06:14:26"),
+  (9, 9, "2020-04-01 03:36:00"),
+  (10, 20, "2017-02-06 08:21:34"),
+  (10, 21, "2017-12-04 05:33:43"),
+  (10, 12, "2017-07-27 05:24:49"),
+  (10, 13, "2017-12-25 01:03:57");
 
 INSERT INTO SpotifyClone.followed_artists (user_id, artist_id)
 VALUES
