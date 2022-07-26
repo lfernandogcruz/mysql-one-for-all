52d741940a655511596162c6430dcb081ddfaf3d
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-07-25 21:05:02 -0300

    fix: change Album's alias from D to R (disc to record)

--- desafio7.sql
+++ desafio7.sql
@@ -1,11 +1,11 @@
 SELECT
 A.artist_name AS artista,
-D.album_title AS album,
+R.album_title AS album,
 COUNT(F.user_id) AS seguidores
 
 FROM SpotifyClone.artists AS A
-RIGHT JOIN SpotifyClone.album AS D
-ON A.artist_id = D.album_artist
+RIGHT JOIN SpotifyClone.album AS R
+ON A.artist_id = R.album_artist
 INNER JOIN SpotifyClone.followed_artists AS F
 ON F.artist_id = A.artist_id
 
